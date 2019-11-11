8ffbfb3fb11ba0cfd0bca9ba2277c68b616827d4
jiangwentao <jiangwentao@xylink.com>
2019-11-11 18:27:47 +0800

    remove unused code

--- src/main/java/com/jwt/study/demo/rest/TestResource.java
+++ src/main/java/com/jwt/study/demo/rest/TestResource.java
@@ -2,8 +2,6 @@ package com.jwt.study.demo.rest;
 
 import generator.Student;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
